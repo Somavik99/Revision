d64e8ca5b1cea03f204dcd26e5a700bbd3a77487
Avik Roy <somavik99@gmail.com>
2023-07-25 13:32:27 +0530

    main

--- src/Components/Update/Update.jsx
+++ src/Components/Update/Update.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Update = () => {
+
+const [UpdateState,setUpdateState] = useState({
+  
+})
+
   return (
     <>
-      <form>
+      <form className="m-lg-5">
         <div>
           <div className="form-group ">
             <label htmlFor="Name">Name</label>
-            <input type="text"  className="form-control"/>
+            <input type="text"  className="form-control w-25 border-4"/>
           </div>
           <div className="form-group">
             <label htmlFor="Email">Email</label>
-            <input type="email"  className="form-control"/>
+            <input type="email"  className="form-control w-25 border-4"/>
           </div>
           <div className="form-group">
             <label htmlFor="Address">Address</label>
-            <input type="text"  className="form-control"/>
+            <input type="text"  className="form-control w-25 border-4"/>
+          </div>
+          <div>
+            <button className="btn btn-outline-success border-2 my-3">Update</button>
           </div>
         </div>
+
       </form>
     </>
   );
