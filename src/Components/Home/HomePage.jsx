cb0fbafe5ae23691c6b9220cd8ee096c05c552c4
Avik Roy <somavik99@gmail.com>
2023-07-25 11:06:31 +0530

    main

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,47 +1,11 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Card from "../Card/Card";
-import "./HomePage.css"
+import React from 'react'
 
 const HomePage = () => {
-  const [dataState, setDataState] = useState([]);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    const FetchApi = async () => {
-      try {
-        const resp = await axios.get(
-          "https://www.googleapis.com/books/v1/volumes?q=search+terms",
-          { signal: controller.signal }
-        );
-        console.log("response", resp.data.items);
-        return setDataState(resp.data.items);
-      } catch (err) {
-        return console.log(err);
-      } finally {
-        return console.log("Data Rendered");
-      }
-    };
-    FetchApi();
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
-
   return (
-    <div className="img__cont">
-      {dataState.map((item, index) => {
-        return (
-          <Card
-            key={index}
-            author={item.volumeInfo.authors}
-            image={item.volumeInfo.imageLinks.thumbnail}
-          />
-        );
-      })}
+    <div>
+        
     </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
